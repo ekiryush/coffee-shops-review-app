83a40023613fbcd841875352f0d23945538792c3
Ekaterina Kiryushina <96707891+ekiryush@users.noreply.github.com>
2023-01-11 17:02:58 -0500

    Commit #14
    Changes to .env and .gitignore files

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ const reviewsRoutes = require("./routes/reviews");
 
 const MongoStore = require("connect-mongo");
 
-// const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://localhost:27017/coffee-shops";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/coffee-shops";
 
 mongoose.connect(dbUrl, {
   useNewURLParser: true,
@@ -44,9 +43,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  secret: "thisshouldbeabettersecret!",
+  secret,
   touchAfter: 24 * 60 * 60,
 });
 
@@ -57,7 +58,7 @@ store.on("error", function (e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
